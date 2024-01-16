5fafa4a4cedf93b2f40cec24a516313ad78505e1
Joe <joeisgood0819@gmail.com>
2024-01-16 14:17:31 +0800

    1/16

---
+++ node04/express07/index.mjs
@@ -0,0 +1,48 @@
+import express from "express";
+import moment from "moment";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+// function log(){}
+let log = (req,res,next)=>{
+app.get("/home", (req, res) => {
+  let { path, ip } = req;
+  let time = moment().format("YYYY-MM-DDTHH:mm:ss");
+  fs.appendFile(
+    resolve(__dirname, "access.log"),
+    `${time} ${ip} ${path}`,
+    () => {})
+  next(); 
+}
+
+app.get("/", (req, res) => {
+  res.send("這是首頁");
+});
+
+app.get("/home", (req, res) => {
+  let { path, ip } = req;
+  let time = moment().format("YYYY-MM-DDTHH:mm:ss");
+  fs.appendFile(
+    resolve(__dirname, "access.log"),
+    `${time} ${ip} ${path}`,
+    () => {}
+  );
+  res.send("這是主頁");
+});
+
+app.get("/login", (req, res) => {
+  res.send("這是登入頁");
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  res.send("<h1>404 - Not Found</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server is running at http://localhost:3000");
+});
